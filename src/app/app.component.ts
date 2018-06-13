4903df037063031a04d05af3fc332fc72a4f9e47
alan.nguyen <alan.nguyen@nexcel.vn>
2018-06-13 14:24:37 +0700

    #1 - Display destinations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,78 +1,36 @@
 import { Component, OnInit, ViewChild, ElementRef, NgZone } from '@angular/core';
-import { FormControl } from '@angular/forms';
 import { MouseEvent, MapsAPILoader } from '@agm/core';
-import { } from 'googlemaps';
+import {} from 'googlemaps';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  zoom: number = 8;  
+  zoom = 8;
   lat: number;
   lng: number;
-  markers: marker[] = [];
+  infoWindow: string;
+  markers: Marker[] = [];
+  dir = null;
+  distance: string;
+  duration: string;
+  position = 0;
+  startElementFocus = true;
 
-  @ViewChild("search") public searchElementRef: ElementRef;
+  @ViewChild('start') public startElement: ElementRef;
+  @ViewChild('end') public endElement: ElementRef;
 
-  constructor(private mapsAPILoader: MapsAPILoader, private ngZone: NgZone){} 
+  constructor(private mapsAPILoader: MapsAPILoader, private ngZone: NgZone) {}
 
-  ngOnInit(){
+  ngOnInit() {
     this.setCurrentPosition();
-    this.addMarker();
     this.loadLocation();
   }
 
-  loadLocation(){
-    this.mapsAPILoader.load().then(() => {
-      let autocomplete = new google.maps.places.Autocomplete(
-        this.searchElementRef.nativeElement, {
-        types: ["address"]
-      });
-      autocomplete.addListener("place_changed", () => {
-        this.ngZone.run(() => {
-          let place: google.maps.places.PlaceResult = autocomplete.getPlace();
-
-          if (place.geometry === undefined || place.geometry === null) {
-            return;
-          }
-
-          this.lat = place.geometry.location.lat();
-          this.lng = place.geometry.location.lng();
-          this.addMarker();
-          this.zoom = 12;
-        });
-      });
-    });
-  }
-
-  addMarker(){
-    this.markers.push({
-      lat: this.lat,
-      lng: this.lng,
-      draggable: true
-    });
-  }
-
-  clickedMarker(label: string, index: number) {
-    console.log(`clicked the marker: ${label || index}`)
-  }
-  
-  mapClicked($event) {
-    this.markers.push({
-      lat: $event.coords.lat,
-      lng: $event.coords.lng,
-      draggable: true
-    });
-  }
-  
-  markerDragEnd(m: marker, $event: MouseEvent) {
-    console.log('dragEnd', m, $event);
-  }
-  
   private setCurrentPosition() {
-    if ("geolocation" in navigator) {
+    if ('geolocation' in navigator) {
       navigator.geolocation.getCurrentPosition((position) => {
         this.lat = position.coords.latitude;
         this.lng = position.coords.longitude;
@@ -80,12 +38,169 @@ export class AppComponent implements OnInit {
       });
     }
   }
+
+  loadLocation() {
+    this.mapsAPILoader.load().then(() => {
+      this.changeStart();
+      this.changeEnd();
+    });
+  }
+
+  changeStart() {
+    const start = new google.maps.places.Autocomplete( this.startElement.nativeElement, { types: ['address'] });
+    start.addListener('place_changed', () => {
+      this.ngZone.run(() => {
+        const place: google.maps.places.PlaceResult = start.getPlace();
+        if (place.geometry === undefined || place.geometry === null) { return; }
+        this.lat = place.geometry.location.lat();
+        this.lng = place.geometry.location.lng();
+        this.infoWindow = this.startElement.nativeElement.value;
+        this.addMarker(0);
+        this.getDirection();
+      });
+    });
+  }
+
+  changeEnd() {
+    const end = new google.maps.places.Autocomplete( this.endElement.nativeElement, { types: ['address'] });
+    end.addListener('place_changed', () => {
+      this.ngZone.run(() => {
+        const place: google.maps.places.PlaceResult = end.getPlace();
+        if (place.geometry === undefined || place.geometry === null) { return; }
+        this.lat = place.geometry.location.lat();
+        this.lng = place.geometry.location.lng();
+        this.infoWindow = this.endElement.nativeElement.value;
+        this.addMarker(1);
+        this.getDirection();
+      });
+    });
+  }
+
+  getDirection() {
+    if (this.markers[0] !== undefined && this.markers[1] !== undefined) {
+      this.dir = {
+        origin: { lat: this.markers[0].lat, lng: this.markers[0].lng },
+        destination: { lat: this.markers[1].lat, lng: this.markers[1].lng }
+      };
+
+      this.getDistance();
+    }
+  }
+
+  async getDistance() {
+    const origin = new google.maps.LatLng(this.markers[0].lat, this.markers[0].lng);
+    const destination = new google.maps.LatLng(this.markers[1].lat, this.markers[1].lng);
+    const request = {
+      origins: [origin],
+      destinations: [destination],
+      travelMode: google.maps.TravelMode.DRIVING
+    };
+
+    const response = await this.getDistanceMatrix(request);
+    const result = response.rows[0].elements[0];
+    this.distance = result.distance.text;
+    this.duration = result.duration.text;
+  }
+
+  private getDistanceMatrix(data) {
+    const service = new google.maps.DistanceMatrixService();
+    return new Promise<google.maps.DistanceMatrixResponse>((resolve, reject) => {
+      service.getDistanceMatrix(data, (response, status) => {
+        if (status === google.maps.DistanceMatrixStatus.OK) {
+          resolve(response);
+        } else {
+          reject(response);
+        }
+      });
+    });
+  }
+
+  private addMarker(position: number) {
+    const item: Marker = {
+      lat: this.lat,
+      lng: this.lng,
+      infoWindow: this.infoWindow,
+      draggable: true
+    };
+
+    if (position === 0) {
+      this.addStart(item);
+    } else {
+      this.addEnd(item);
+    }
+
+    this.markers = this.markers;
+  }
+
+  private addStart(item: Marker) {
+    item.label = 'A';
+    if (this.markers.length > 0) {
+      this.markers[0] = item;
+    } else {
+      this.markers.push(item);
+    }
+  }
+
+  private addEnd(item: Marker) {
+    item.label = 'B';
+    if (this.markers.length === 2) {
+      this.markers[1] = item;
+    } else {
+      this.markers.push(item);
+    }
+  }
+
+  clickedMarker(label: string, index: number) {
+    console.log(`clicked the marker: ${label || index}`);
+  }
+
+  markerDragEnd(m: Marker, $event: MouseEvent) {
+    console.log('dragEnd', m, $event);
+  }
+
+  async mapClicked($event) {
+    this.lat = $event.coords.lat;
+    this.lng = $event.coords.lng;
+    await this.getInfoWindow();
+    this.addMarker(this.position);
+    this.updateInput();
+    this.markers = this.markers;
+    this.getDirection();
+  }
+
+  async getInfoWindow() {
+    const result = await this.geocodeLatLng();
+    this.infoWindow = result.formatted_address;
+  }
+
+  private geocodeLatLng(): Promise<google.maps.GeocoderResult> {
+    const geocoder = new google.maps.Geocoder;
+    const latlng = {lat: this.lat, lng: this.lng};
+    return new Promise<google.maps.GeocoderResult>((resolve, reject) => {
+      geocoder.geocode({'location': latlng}, function(results, status) {
+        if (status === google.maps.GeocoderStatus.OK && results[0]) {
+          resolve(results[0]);
+        } else {
+          reject();
+        }
+      });
+    });
+  }
+
+  updateInput() {
+    if (this.position === 0) {
+      this.endElement.nativeElement.focus();
+      this.startElement.nativeElement.value = this.infoWindow;
+    } else {
+      this.endElement.nativeElement.value = this.infoWindow;
+    }
+  }
 }
 
-// just an interface for type safety.
-interface marker {
-	lat: number;
-	lng: number;
-	label?: string;
-	draggable: boolean;
+interface Marker {
+ lat: number;
+ lng: number;
+ label?: string;
+ infoWindow?: string;
+ draggable: boolean;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AgmCoreModule } from '@agm/core';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
+import { AgmDirectionModule } from 'agm-direction';
 
 
 @NgModule({
@@ -18,7 +19,8 @@ import { ReactiveFormsModule } from '@angular/forms';
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyDuxR3jyTgjHRqresdUYtGD4Sx9_wtmaeA',
       libraries: ['places']
-    })
+    }),
+    AgmDirectionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
